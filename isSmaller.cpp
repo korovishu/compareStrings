574043c243b7cee8ac942bd3d364ab0389476c19
korovishu <50824297+korovishu@users.noreply.github.com>
2019-07-30 16:32:19 +0530

    Update isSmaller.cpp

--- isSmaller.cpp
+++ isSmaller.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 // SUBTRACTION
 #define pb push_back;
-#define f(i, a, b) for(i=a; i<b; i++);
 bool small(string s1, string s2)
 {
     int n1=s1.length(); int n2=s2.length();
@@ -10,8 +9,7 @@ bool small(string s1, string s2)
     if(n1>n2) return false;
     if(n1==n2)
     {
-        int i;
-        f(i, 0, n1)
+        for(int i=0; i<n1; i++)
         {
             if((s1[i]-'0')>(s2[i]-'0')) return false;
             if((s1[i]-'0')<(s2[i]-'0')) return true;
